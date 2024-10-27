bfafa5c5feb07fa3b534ca17faa217cac11dda34
Reicsen <147541859+Reicsen@users.noreply.github.com>
2024-10-27 22:25:50 +0100

    Obsługa rejestracji i tworzenia projektu

--- CzescUzytkownika/TworzenieProjektu.py
+++ CzescUzytkownika/TworzenieProjektu.py
@@ -1,41 +1,46 @@
 import socket
 import typing
+import hashlib as hash
 import ManagerHasel as Hasla
+import ManagerKodow as Kody
 import ManagerNazw as Nazwy
 
 
-def analizaOdpowiedzi(odpowiedz: bytes) -> typing.Tuple[bool, str, str]:                    #TODO
-    #zmiana odpowiedzi serwera w [False,"",""] lub [True, odszyfrowany token sesji, rola]
-    return [False,"",""]    #tymczasowo
+def analizaOdpowiedzi(odpowiedz: bytes) -> typing.Tuple[bool, str]:                    #TODO
+    #zmiana odpowiedzi serwera w [False,""] lub [True, odszyfrowany token sesji]
+    return [False,""]    #tymczasowo
 
 
-def probaLogowania(adresSerwera: typing.Tuple[str,int], projekt: str, login: str, haslo: str) -> typing.Tuple[str, str]:    #w przypadku sukcesu zwraca token sesji oraz (dla celów wizualnych) rolę w projekcie
+def probaRejestracji(adresSerwera: typing.Tuple[str,int], projekt: str, kodZapr: str, login: str, haslo: str, powtHaslo: str) -> typing.Tuple[str, str]:    #w przypadku sukcesu zwraca token sesji oraz (dla celów wizualnych) rolę w projekcie
     if (projekt=="" or login=="" or haslo==""):
         raise NameError("PustePole")
+    if(haslo!=powtHaslo):
+        raise NameError("RozneHasla")
     if (not Nazwy.przetestujNazwe(projekt)):
         raise NameError("ZlaNazwaProjektu")
     if (not Nazwy.przetestujNazwe(login)):
         raise NameError("ZlyLogin")
+    if(not Kody.przetestujKod(kodZapr)):
+        raise NameError("ZlyKod")
     if (not Hasla.czyBrakZabronionychZnakow(haslo)):
         raise NameError("ZlyZnakWHasle")
+    if (not Hasla.poprawnoscHasla(haslo)):
+        raise NameError("ZleHaslo")
     
     try:
         serwer: socket.socket = socket.create_connection(adresSerwera)
         serwer.sendall(projekt)                                             #TODO póżniej zmienić w rzeczywistą wersję
         czyProjektIstnieje: bool = bool(serwer.recv(4096))                  #TODO póżniej zmienić w rzeczywistą wersję
         
-        if(not czyProjektIstnieje):
-            raise NameError("__ProjNieIstnieje")
+        if(czyProjektIstnieje):
+            raise NameError("__ProjIstnieje")
         
-        serwer.sendall(login,haslo)                                         #TODO póżniej zmienić w rzeczywistą i zaszyfrowaną wersję
+        serwer.sendall(hash.sha3_512(login),hash.sha3_512(haslo))           #TODO póżniej zmienić w rzeczywistą i dodatkowo zaszyfrowaną wersję
         odpowiedz: bytes = serwer.recv(4096)
         rezultat: typing.Tuple = analizaOdpowiedzi(odpowiedz)
         
-        if(not rezultat[0]):
-            raise NameError("__NieudaneLogowanie")
-        else:
-            serwer.close
-            return rezultat[1], rezultat[2]
+        serwer.close()
+        return rezultat[1], "Właściciel zespołu"
     
     except NameError as error:
         if(str(error)[:2]=="__"):
