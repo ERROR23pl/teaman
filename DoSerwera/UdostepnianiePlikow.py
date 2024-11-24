beb0415c160b9018e87cc547aa93ddc3d7ddbbcf
Reicsen <reicsen@gmail.com>
2024-11-24 20:14:25 +0100

    Dokończenie obsługi udost. plików

--- DoSerwera/UdostepnianiePlikow.py
+++ DoSerwera/UdostepnianiePlikow.py
@@ -56,15 +56,44 @@ def usunPlik(login: str, token: str, nazwaPokoju: str, nazwaPliku: str, zawartos
                 return True, True, True         #jeśli wpis nie nie istniał, to usunięcie zostaje uznane za udane
 
             if (Bazy.iloscUzytkownikow(login=hashLog,token=hashTok,rola="Właściciel zespołu")==1 or Bazy.autorPliku(nazwaPokoju,nazwaPliku)==hashLog):  #TODO
-                Bazy.usunPlik(nazwaPokoju,nazwaPliku)       #TODO
+                Bazy.usunPlik(login,token,nazwaPokoju,nazwaPliku)       #TODO
                 return True, True, True
             
             return True, True, False
 
 
 
+def pobierzPlik(login: str, token: str, nazwaPokoju: str, nazwaPliku: str) -> typing.Tuple[bool, bool, bool, str]: #[czy poprawne dane, czy pokój istniał i się do niego należy, czy plik o takiej nazwie istniał, decode zawartości pliku]
+    hashLog: str = hash.sha3_512(login.encode()).hexdigest()
+    hashTok: str = hash.sha3_512(token.encode()).hexdigest()
+    
+    wynik: int = Bazy.iloscUzytkownikow(login=hashLog, token=hashTok)
+    
+    if(wynik!=1):
+        return False, False, False, ""
+        
+    czyPokojIstnieje: bool = Bazy.czyJestPokoj(nazwaPokoju)
+    
+    if(not czyPokojIstnieje):
+        return True, False, False, ""
+    
+    else:
+        czyNalezyDoPokoju: bool = Bazy.czyUzytkownikJestWPokoju(nazwaPokoju,hashLog)
+        if(not czyNalezyDoPokoju):
+            return True, False, False, ""
+        
+        else:
+            czyPlikJest: bool = Bazy.czPlikIstnieje(nazwaPokoju,nazwaPliku)
+            if (not czyPlikJest):
+                return True, True, False, ""
+            
+            zawartosc: bytes = Bazy.pobierzPlik(login,token,nazwaPokoju,nazwaPliku)       #TODO
+            strZawartosci: str = zawartosc.decode()
+            return True, True, True, strZawartosci
 
-def pobierzKalendarz(login: str, token: str, nazwaPokoju: str) -> typing.Tuple[bool, bool, typing.List[str]]: #[czy poprawne dane, czy pokój istniał i się do niego należy, kalendarz pokoju w formie listy stringów]
+
+
+def pobierzListePlikow(login: str, token: str, nazwaPokoju: str) -> typing.Tuple[bool, bool, typing.List[str]]: #[czy poprawne dane, czy pokój istniał i się do niego należy, lista plików (nazwa, autor) w pokoju w formie listy stringów]
     hashLog: str = hash.sha3_512(login.encode()).hexdigest()
     hashTok: str = hash.sha3_512(token.encode()).hexdigest()
     
@@ -84,5 +113,5 @@ def pobierzKalendarz(login: str, token: str, nazwaPokoju: str) -> typing.Tuple[b
             return True, False, [""]
         
         else:
-            lista: typing.List[str] = Bazy.pobierzKalendarz(login,token,nazwaPokoju)
+            lista: typing.List[str] = Bazy.listaPlikow(login,token,nazwaPokoju) #TODO
             return True, True, lista
